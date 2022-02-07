7c318b6f9749971898c5397437f9334829d9aafe
kim_jeongyeon <wjddus9132@gmail.com>
2022-02-07 18:48:30 +0900

    Multiple Path

--- src/App.js
+++ src/App.js
@@ -5,17 +5,8 @@ import Home from "./Home";
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-      </ul>
-      <hr />
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
     </div>
   );
 };
